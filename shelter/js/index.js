023cb8b9469599525b108a6414c9eb51ef88884f
Mariya Fain <fainmariya@gmail.com>
2026-06-15 21:16:44 +0300

    feat: add responsive layout for shelter part 2

---
+++ shelter/js/index.js
@@ -0,0 +1,19 @@
+console.log(`
+    Shelter Part 2 self-assessment:
+    
+    Main page:
+    - Layout at 1280px
+    - Layout at 768px
+    - Layout at 320px
+    
+    Pets page:
+    - Layout at 1280px
+    - Layout at 768px
+    - Layout at 320px
+    
+    Responsive behavior:
+    - No horizontal scroll from 320px to 1280px
+    - Smooth adaptation between breakpoints
+    - Burger icon appears below 768px
+    - Markup is valid
+    `);
